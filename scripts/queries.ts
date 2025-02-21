b3fb1f0c0ec8fb4aeba66f8dc34e9b80b295ae1e
vnFuhung2903 <hung29032004@gmail.com>
2025-02-22 01:10:39 +0700

    add queries and cctp scripts

---
+++ scripts/queries.ts
@@ -0,0 +1,42 @@
+import { PerChainQueryRequest, QueryProxyMock, QueryRequest, QueryResponse, SolanaPdaQueryRequest, SolanaPdaQueryResponse } from "@wormhole-foundation/wormhole-query-sdk"
+import { Program } from "@coral-xyz/anchor";
+import IDL from "../program/market.json";
+import { ForecastMarket } from "../program/market";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const forecastMarketProgram = new Program(IDL as ForecastMarket, {
+    connection: connection,
+});
+const marketKey = Buffer.alloc(8);
+marketKey.writeBigUInt64LE(1n);
+const marketSeed = Buffer.from("market", 'utf-8');
+const seeds = [marketSeed, marketKey];
+
+
+const main = async () => {
+    const mock = new QueryProxyMock({
+        1: clusterApiUrl("devnet"),
+    });
+
+    const query = new QueryRequest(42, [
+        new PerChainQueryRequest(
+          1,
+          new SolanaPdaQueryRequest("finalized", [
+            {
+              programAddress: forecastMarketProgram.programId.toBytes(),
+              seeds: seeds.map((seed) => seed),
+            },
+          ])
+        ),
+      ]);
+      
+    const resp = await mock.mock(query);
+    
+    const queryResponse = QueryResponse.from(Buffer.from(resp.bytes, "hex"));
+    const solResponse = queryResponse.responses[0].response as SolanaPdaQueryResponse;
+    console.log(queryResponse.responses[0].response);
+}
+
+main();
